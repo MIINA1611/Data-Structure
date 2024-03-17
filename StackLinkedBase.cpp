ebaaa96b920058352a35224f13c9c05f2f5731fc
MINA <aymon74442@gmail.com>
2024-03-18 00:30:59 +0200

    Task 2 Stack Using Linked Base

--- StackLinkedBase.cpp
+++ StackLinkedBase.cpp
@@ -1,181 +1,207 @@
 #include <iostream>
-#include <string>
 using namespace std;
-template <class Type>
+template <class T>
 class Node
 {
 public:
-    Type data;
-    Node<Type> *next;
+    T data;
+    Node<T> *next;
     Node()
     {
         data = 0;
-        next = NULL;
+        next = nullptr;
     }
 };
-template <class Type>
-class stackADT
+template <class T>
+class Stack
 {
-    Node<Type> *top;
-    void copyStack(const stackADT<Type> &other);
+    Node<T> *top;
+    void copyStack(const Stack<T> &other)
+    {
+        Node<T> *newnode, *current, *last;
+        clearStack();
+        current = other.top;
+        last = nullptr;
+        while (current != nullptr)
+        {
+            newnode = new Node<T>();
+            newnode->data = current->data;
+            newnode->next = nullptr;
+            if (top == nullptr)
+                top = newnode;
+            else
+                last->next = newnode;
+            last = newnode;
+            current = current->next;
+        }
+    }
 
 public:
-    stackADT();                            // defulte constructor
-    void initialize();                     // TOP=NULL
-    bool isEmpty();                        // Check Stack IS EMPTY Or NOOT
-    bool isFullStack();                    // RETURN FALSE FOREVER
-    void push(const Type &Newitem);        // THIS IS PUSH
-    Type Top();                            // RETURN DATA FOR TOP
-    void pop();                            // DELETE DATA FOR TOP
-    void clearStack();                     // CLEAR ALL STACK
-    int count();                           // THE ELEMENT STACK COUNTER
-    void Traverse();                       // PRINT ALL THE ELEMENT STACK
-    stackADT(const stackADT<Type> &other); // COPY STACK TO ANOTHER STACK
-                                           //  bool operator=(const stackADT<Type> &x); // CHECK WHETHER FIRST STACK EQUAL ANOTHER STACK OR NOT
-    ~stackADT();                           // DELETE TOP
+    Stack()
+    {
+        top = nullptr;
+    }
+    void initialize()
+    {
+        while (top != nullptr)
+        {
+            Node<T> *delptr = top;
+            top = top->next;
+            delete delptr;
+        }
+    }
+    bool isEmpty()
+    {
+        return (top == nullptr);
+    }
+    bool isFull()
+    {
+        return false;
+    }
+    void push(const T item)
+    {
+        Node<T> *newnode = new Node<T>();
+        newnode->data = item;
+        if (isEmpty())
+        {
+            newnode->next = nullptr;
+            top = newnode;
+        }
+        else
+        {
+            newnode->next = top;
+            top = newnode;
+        }
+    }
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "The Stack is Empty!" << endl;
+        }
+        else
+        {
+            Node<T> *delptr = top;
+            top = top->next;
+            delete delptr;
+        }
+    }
+    void Traverse()
+    {
+        Node<T> *temp = top;
+        while (temp != nullptr)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    void clearStack()
+    {
+        initialize();
+    }
+    int Stacksize()
+    {
+        int counter;
+        Node<T> *temp = top;
+        while (temp != nullptr)
+        {
+            counter++;
+            temp = temp->next;
+        }
+        return counter;
+    }
+    T Top()
+    {
+        return top->data;
+    }
+    Stack<T> &operator=(const Stack<T> &other)
+    {
+        if (this != &other)
+            copyStack(other);
+        return *this;
+    }
+    friend bool operator==(const Stack<T> &firstStack, const Stack<T> &secondStack)
+    {
+        Node<T> *temp1 = firstStack.top;
+        Node<T> *temp2 = secondStack.top;
+        while (temp1 != nullptr && temp2 != nullptr)
+        {
+            if (temp1->data != temp2->data)
+                return false;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        return true;
+    }
+    void merge(const Stack<T> &first, const Stack<T> &other)
+    {
+        Node<T> *temp1 = first.top;
+        while (temp1 != nullptr)
+        {
+            push(temp1->data);
+            temp1 = temp1->next;
+        }
+        Node<T> *temp2 = other.top;
+        while (temp2 != nullptr)
+        {
+            push(temp2->data);
+            temp2 = temp2->next;
+        }
+    }
+    Stack<T> operator+(const Stack<T> &other) const
+    {
+        Stack<T> result;
+        Node<T> *temp1 = this->top;
+        Node<T> *temp2 = other.top;
+        int carry = 0;
+
+        while (temp1 != NULL || temp2 != NULL || carry != 0)
+        {
+            int sum = carry;
+            if (temp1 != NULL)
+            {
+                sum += temp1->data;
+                temp1 = temp1->next;
+            }
+            if (temp2 != NULL)
+            {
+                sum += temp2->data;
+                temp2 = temp2->next;
+            }
+            carry = sum / 10;
+            sum %= 10;
+            result.push(sum);
+        }
+
+        return result;
+    }
+    ~Stack()
+    {
+        delete top;
+    }
 };
-template <class Type>
-stackADT<Type>::stackADT(){};
-template <class Type>
-void stackADT<Type>::initialize()
-{
-    top = NULL;
-}
-template <class Type>
-bool stackADT<Type>::isEmpty()
-{
-    return (top == NULL);
-}
-template <class Type>
-bool stackADT<Type>::isFullStack()
-{
-    return false;
-}
-template <class Type>
-void stackADT<Type>::push(const Type &newitem)
-{
-    Node<Type> *newnode = new Node<Type>();
-    newnode->data = newitem;
-    if (isEmpty())
-    {
-        newnode->next = NULL;
-        top = newnode;
-    }
-    else
-    {
-        newnode->next = top;
-        top = newnode;
-    }
-}
-template <class Type>
-Type stackADT<Type>::Top()
-{
-    return top->data;
-}
-template <class Type>
-void stackADT<Type>::pop()
-{
-    Node<Type> *delptr = top;
-    top = top->next;
-    delete delptr;
-}
-template <class Type>
-int stackADT<Type>::count()
-{
-    Node<Type> *temp = top;
-    int counter = 0;
-    while (temp != NULL)
-    {
-        counter++;
-        temp = temp->next;
-    }
-    return counter;
-}
-template <class Type>
-void stackADT<Type>::clearStack()
-{
-    while (top != NULL)
-    {
-        Node<Type> *delptr = top;
-        top = top->next;
-        delete delptr;
-    }
-}
-template <class Type>
-void stackADT<Type>::Traverse()
-{
-    Node<Type> *temp = top;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-    cout << endl;
-}
-template <class Type>
-void stackADT<Type>::copyStack(const stackADT<Type> &other)
-{
-    top = other.top;
-}
-// template<class Type> bool stackADT<Type>::operator=(const stackADT<Type> &x)
-// {
-//     if (x.top != top)
-//     {
-//         return false;
-//     }
-//     else if (x.count() != count())
-//     {
-//         return false;
-//     }
-//     else
-//     {
-//         Node<Type> *temp1 = x.top;
-//         Node<Type> *temp2 = top;
-//         while (temp1 != NULL)
-//         {
-//             if (temp1 != temp2)
-//             {
-//                 return false;
-//             }
-//             temp1 = temp1->next;
-//             temp2 = temp2->next;
-//         }
-//         return true;
-//     }
-// }
-template <class Type>
-stackADT<Type>::stackADT(const stackADT &other)
-{
-    copyStack(other);
-}
-template <class Type>
-stackADT<Type>::~stackADT()
-{
-    delete top;
-}
 int main()
 {
-    stackADT<int> x;
-    x.initialize();
+    Stack<int> x;
     x.push(1);
     x.push(2);
     x.push(3);
-    x.push(4);
-    x.push(5);
-    cout << x.count() << endl;
-    // x.Traverse();
-    // x.pop();
-    // x.Traverse();
-    // x.clearStack();
-    // if (x.isEmpty())
-    // {
-    //     cout << "Stack is Clear" << endl;
-    // }
-    // else
-    // {
-    //     cout << "Clear This Stack Idiot!" << endl;
-    // }
-    stackADT<int> y(x);
-    y.pop();
+    Stack<int> y;
+    y.push(4);
+    y.push(4);
+
+    y.push(4);
+
+    y.push(4);
+
     y.Traverse();
+    if (x == y)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+    Stack<int> MergeStack;
+    MergeStack.merge(x, y);
+    // MergeStack=x+y;
+    MergeStack.Traverse();
 }
